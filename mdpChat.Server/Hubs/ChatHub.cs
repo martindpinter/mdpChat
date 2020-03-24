90679ce048118c40fbc43e386763c86261180d6b
martindpinter <martindpinter@gmail.com>
2020-03-24 18:40:55 +0200

    Renamed DataManager(IDataManager) instance DataStore to _db in ChatHub.cs

--- mdpChat.Server/Hubs/ChatHub.cs
+++ mdpChat.Server/Hubs/ChatHub.cs
@@ -14,10 +14,10 @@ namespace mdpChat.Server
     public class ChatHub : Hub
     {
         private readonly string _globalChatRoomName = "General"; // move to config?
-        private readonly IDataManager DataStore;
-        public ChatHub(IDataManager dataStore)
+        private readonly IDataManager _db;
+        public ChatHub(IDataManager dataManager)
         {
-            DataStore = dataStore;
+            _db = dataManager;
         }
 
         #region Public, directly invokable methods (via SignalR connections)
@@ -26,12 +26,12 @@ namespace mdpChat.Server
         {
             await base.OnConnectedAsync();
 
-            DataStore.HandleConnection(Context.ConnectionId);
+            _db.HandleConnection(Context.ConnectionId);
         }
 
         public async override Task OnDisconnectedAsync(Exception ex)    // if Exception is null, termination was intentional
         {
-            DataStore.HandleDisconnection(Context.ConnectionId);
+            _db.HandleDisconnection(Context.ConnectionId);
             await base.OnDisconnectedAsync(ex);
 
             // SignalR automatically removes disconnected ConnectionIds from SignalR Groups on Disconnect
@@ -39,14 +39,13 @@ namespace mdpChat.Server
 
         public async Task OnLogin(string userName)
         {
-            DataStore.HandleLogin(Context.ConnectionId, userName);
-
+            _db.HandleLogin(Context.ConnectionId, userName);
             await OnJoinGroup(userName, _globalChatRoomName);
         }
 
         public async Task OnCreateGroup(string groupName)
         {
-            OperationResult res = DataStore.HandleCreateGroup(groupName);
+            OperationResult res = _db.HandleCreateGroup(groupName);
 
             if (res.Successful)
             {
@@ -61,11 +60,11 @@ namespace mdpChat.Server
 
         public async Task OnDeleteGroup(string groupName)
         {
-            OperationResult res = DataStore.HandleDeleteGroup(groupName);
+            OperationResult res = _db.HandleDeleteGroup(groupName);
 
             if (res.Successful)
             {
-                List<Client> clientsInGroup = DataStore.GetClientsInGroup(groupName);
+                List<Client> clientsInGroup = _db.GetClientsInGroup(groupName);
                 foreach (Client client in clientsInGroup)
                 {
                     await Groups.RemoveFromGroupAsync(client.ConnectionId, groupName);
@@ -79,7 +78,7 @@ namespace mdpChat.Server
 
         public async Task OnJoinGroup(string userName, string groupName) // none of these will be async, probably
         {
-            OperationResult res = DataStore.HandleJoinGroup(userName, groupName);
+            OperationResult res = _db.HandleJoinGroup(userName, groupName);
             
             if (res.Successful)
             {
@@ -90,8 +89,8 @@ namespace mdpChat.Server
                 await ReturnErrorMessage(res.ErrorMessage);
             }
 
-            Group group = DataStore.GetGroup(groupName);
-            List<Message> msgList = DataStore.GetAllMessagesInGroup(group.Name);
+            Group group = _db.GetGroup(groupName);
+            List<Message> msgList = _db.GetAllMessagesInGroup(group.Name);
             string serialized = JsonSerializer.Serialize(msgList);
 
             await Clients.Caller.SendAsync("ReceiveMessagesOfGroup", groupName, serialized);
@@ -101,7 +100,7 @@ namespace mdpChat.Server
 
         public async Task OnLeaveGroup(string userName, string groupName)
         {
-            OperationResult res = DataStore.HandleLeaveGroup(userName, groupName);
+            OperationResult res = _db.HandleLeaveGroup(userName, groupName);
 
             if (res.Successful)
             {
@@ -115,29 +114,27 @@ namespace mdpChat.Server
 
         public async Task OnSendMessageToGroup(string groupName, string message)
         {
-            DataStore.HandleSendMessageToGroup(groupName, message, Context.ConnectionId);
-            string authorName = DataStore.GetUserAttached(Context.ConnectionId).Name;
+            _db.HandleSendMessageToGroup(groupName, message, Context.ConnectionId);
+            string authorName = _db.GetUserAttached(Context.ConnectionId).Name;
             await Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", authorName, groupName, message);
         }
 
         public async Task OnGetUsersInGroup(string groupName)
         {
-            List<User> userList = DataStore.GetUsersInGroup(groupName);
+            List<User> userList = _db.GetUsersInGroup(groupName);
             string serialized = JsonSerializer.Serialize(userList);
             await Clients.Caller.SendAsync("ReceiveUsersInGroup", groupName, serialized);
         }
 
         public async Task OnGetGroupsList()
         {
-            List<Group> res = DataStore.GetAllGroups();
+            List<Group> res = _db.GetAllGroups();
             string serialized = JsonSerializer.Serialize(res);
             await Clients.Caller.SendAsync("ReceiveGroupsList", serialized);
         }
-
         #endregion
 
         #region Private, directly not invokable methods (via SignalR connections)
-
         private async Task ReturnErrorMessage(string errorMessage)
         {
             await Clients.Caller.SendAsync("ReceiveErrorMessage", errorMessage);
